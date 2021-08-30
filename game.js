aebd3c8d27dd6bc3e636d9e8271f910ec92c316e
Minchan Jung <minchanjung1@gmail.com>
2021-08-30 21:52:45 +0100

    working buttons, added headers and text

--- game.js
+++ game.js
@@ -1,73 +1,97 @@
 //lets the game know who won each round
-let score;
 
-//the function for the ai
+const computerSelection = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+
+
 function computerPlay() {
     let rps = ["Rock", "Paper", "Scissor"];
     let random = Math.floor(Math.random() * rps.length);
     return rps[random];
 }
 
-const computerSelection = computerPlay();
-console.log(computerSelection)
-
 //function to play a single round of rps
 function playRound(playerSelection, computerSelection) {
     let playerSelect = playerSelection.toLowerCase();
     if (playerSelect === "rock" && computerSelection === "Rock") {
-        score = 0;
+        
         return "It was a tie!";
     } else if (playerSelect === "rock" && computerSelection === "Paper") {
-        score = false;
+        computerScore += 1;
         return "You lose, paper beats rock!";
         
     } else if (playerSelect === "rock" && computerSelection === "Scissor") {
-        score = true;
+        playerScore += 1;
         return "You win, rock beats scissor!";
         
     } else if (playerSelect === "paper" && computerSelection === "Rock") {
-        score = true;
+        playerScore += 1;
         return "You win, paper beats rock!";
         
     } else if (playerSelect === "paper" && computerSelection === "Paper") {
-        score = 0;
+        
         return "It was a tie!";
         
     } else if (playerSelect === "paper" && computerSelection === "Scissor") {
-        score = false;
+        computerScore += 1;
         return "You lose, scissor beats paper!";
         
     } else if (playerSelect === "scissor" && computerSelection === "Rock") {
-        score = false
+        computerScore += 1;
         return "You lose, rock beats scissor!";
         
     } else if (playerSelect === "scissor" && computerSelection === "Paper") {
-        score = true;
+        playerScore += 1;
         return "You win, scissor beats paper!";
         
     } else {
-        score = 0;
+        
         return "It was a tie!";
         
     }
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+    const output = document.querySelector('#output');
+    output.textContext = 'Choose Rock, Paper, or Scissor';
+
+    const player = document.querySelector('#playerScore');
+    player.textContent = `Player Score: ${playerScore}`
+
+    const computer = document.querySelector('#computerScore');
+    computer.textContent = `Computer Score: ${computerScore}`
+
     const rockBtn = document.querySelector('button');
     const paperBtn = document.querySelector('.paper');
     const scissorBtn = document.querySelector('.scissor');
 
+
     rockBtn.addEventListener('click', function() {
-        console.log(playRound('rock', computerSelection));
+        output.textContent = playRound('rock', computerSelection);
+        player.textContent = `Player Score: ${playerScore}`
+        computer.textContent = `Computer Score: ${computerScore}`
     });
     paperBtn.addEventListener('click', function() {
-        console.log(playRound('paper', computerSelection));
+        output.textContent = playRound('paper', computerSelection);
+        player.textContent = `Player Score: ${playerScore}`
+        computer.textContent = `Computer Score: ${computerScore}`
     });
     scissorBtn.addEventListener('click', function() {
-        console.log(playRound('scissor', computerSelection));
+        output.textContent = playRound('scissor', computerSelection);
+        player.textContent = `Player Score: ${playerScore}`
+        computer.textContent = `Computer Score: ${computerScore}`
     });
+
+    if (playerScore === 5) {
+        output.textContent = "Congratulations You Have Beaten the Computer!";
+        playerScore = 0;
+        computerScore = 0;
+    } else if (computerScore === 5) {
+        output.textContent = "You Lose, The Computer Has Beaten You!";
+        playerScore = 0;
+        computerScore = 0;
+    }
 }
 
 game()
